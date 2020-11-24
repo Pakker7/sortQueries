422e5d41e369964ccb34a5a25c78fec700fa097f
psk <pakker4321@itnomads.com>
2020-11-24 17:43:41 +0900

    프로젝트 생성

---
+++ src/main/java/com/psk/sortqueries/SortqueriesApplication.java
@@ -0,0 +1,13 @@
+package com.psk.sortqueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortqueriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortqueriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psk/sortqueries/SortqueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psk.sortqueries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortqueriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
